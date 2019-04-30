bf787ea90c8bb96162da6a6306e2aafe4e40c4ae
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-04-30 22:04:01 +1000

    added codes to seeds.rb

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 # Authorization
 gem 'cancancan'
+# Faker
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Start of Seeding..."
+User.destroy_all
+10.times do
+  params = {
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  }
+  puts "Creating User: #{params[:email]}"
+  user = User.new(params)
+  user.save
+
+  listings = rand(6)
+  listings.times do
+    params = {
+      title: Faker::WorldCup.group,
+      description: Faker::Lorem.paragraph,
+      amount: Faker::Number.decimal(6, 2),
+      user_id: user.id
+    }
+    puts "Creating listing: #{params[:title]}"
+    listing = Listing.new(params)
+    listing.save
+
+    offers = rand(3)
+    offers.times do
+      params = {
+        interest: Faker::Number.decimal(1, 2),
+        description: Faker::Lorem.paragraph,
+        user_id: user.id,
+        listing_id: listing.id
+      }
+      puts "Creating offer: #{params[:interest]}"
+      offer = Offer.new(params)
+      offer.save
+    end
+  end
+end
+
+puts "Seeding Over"
